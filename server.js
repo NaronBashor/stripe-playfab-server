9fb2b16b497dfcc641a2d141a6866e84de9238ed
NaronBashor <139496680+NaronBashor@users.noreply.github.com>
2025-04-02 17:50:42 -0700

    Update server.js
    Updating urls to my site for security and testing

--- server.js
+++ server.js
@@ -21,8 +21,8 @@ app.post('/create-checkout-session', async (req, res) => {
                 quantity: 1,
             }],
             customer_email: email,
-            success_url: `https://stripe-playfab-server.onrender.com/success?playfabId=${playFabId}`,
-            cancel_url: 'https://stripe-playfab-server.onrender.com/cancel',
+            success_url: `https://splitrockgames.com/tarkovto-do`,
+            cancel_url: 'https://splitrockgames.com/tarkovto-do',
         });
 
         res.json({ url: session.url });
